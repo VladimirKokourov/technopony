c4f6fd45856e75797bb63d0cb76196e54c97a324
vkoko <vkokourov@gmail.com>
2022-07-08 12:26:40 +0700

    Initial commit

---
+++ src/main/java/ru/vkokourov/technopony/TechnoponyApplication.java
@@ -0,0 +1,13 @@
+package ru.vkokourov.technopony;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TechnoponyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TechnoponyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/vkokourov/technopony/TechnoponyApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.vkokourov.technopony;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TechnoponyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
